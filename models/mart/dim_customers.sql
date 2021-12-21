cd772f6d33975edf56afe4fb2f9ed89b32bb2874
Domenico Boris Salvati <domenicoboris.salvati@yahoo.it>
2021-12-21 10:15:08 +0000

    final review

--- models/mart/dim_customers.sql
+++ models/mart/dim_customers.sql
@@ -1,61 +1,29 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
-
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    select * from {{ ref('fct_orders')}}
 ),
-
-lifetime_value as (
-
-    select 
-        customers.*,
-        sum(stg_payments.amount) as lifetime_value
-    
-    from customers
-    left join orders
-    on customers.customer_id = orders.customer_id
-    left join {{ ref('stg_payments') }}
-    on orders.order_id = stg_payments.orderid
-
-    group by 1, 2, 3
-
-),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
 ),
-
 final as (
-
     select
-        lifetime_value.customer_id,
-        lifetime_value.first_name,
-        lifetime_value.last_name,
-        lifetime_value.lifetime_value,
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from lifetime_value
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    from customers
     left join customer_orders using (customer_id)
-
 )
-
 select * from final
--- models/mart/fct_orders.sql
+++ models/mart/fct_orders.sql
@@ -1,12 +1,30 @@
-with cte as (
-    select 
-        stg_orders.order_id,
-        stg_orders.customer_id,
-        stg_payments.amount
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
 
-    from {{ ref('stg_orders') }}
-    left join {{ ref('stg_payments') }}
-    on stg_orders.order_id = stg_payments.orderid
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select * from cte
+select * from final
